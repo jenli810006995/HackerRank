c2766fe32ba6b5c2d811e4ce03dc589f19a8b3a4
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-03-28 09:34:44 -0400

    Add files via upload

--- Algorithms/BirthdayCakeCandles.py
+++ Algorithms/BirthdayCakeCandles.py
@@ -3,13 +3,13 @@ def birthdayCakeCandles(ar):
     tallest=0
     max_value=max(ar)
     for i in range(0, n):
-        if ar.split(' ')[i]==max_value:
+        if int(ar[i])==max_value:
             tallest+=1
         else:
             tallest+=0
     return tallest    
     
 n=int(input().strip()) # number of candles
-ar=input().strip() # array of numbers
+ar=list(map(int, input().split())) # array of numbers
 result=birthdayCakeCandles(ar)
 print(result)
