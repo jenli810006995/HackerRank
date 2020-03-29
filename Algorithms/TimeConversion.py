66c724a2dcf508702c176d60c4c9b211c8b4ed6b
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-03-29 10:30:07 -0400

    Add files via upload

---
+++ Algorithms/TimeConversion.py
@@ -0,0 +1,14 @@
+def timeConversion(s):
+    time = s.split(":")
+    if s[-2:] == "PM":
+        if time[0] != "12":
+            time[0] = str(int(time[0])+12)
+    else:
+        if time[0] == "12":
+            time[0] = "00"
+    ntime = ':'.join(time)
+    return str(ntime[:-2])
+
+s=str(input().strip())
+result=timeConversion(s)
+print(result)
